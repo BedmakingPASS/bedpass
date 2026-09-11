26277b5dbb9a481909deec9d30f1c4005da4a8c5
hannanazhifarp-code <hannanazhifarp@upi.edu>
2026-09-11 20:16:43 +0700

    update base url dan config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: '/WebsitePertama/', // <--- Tambahkan baris ini (sesuai nama repository GitHub Anda)
 })
