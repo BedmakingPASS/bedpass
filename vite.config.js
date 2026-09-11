77fa0eabd7cf9b4ed11b52f0e178656f0129e398
hannanazhifarp-code <hannanazhifarp@upi.edu>
2026-09-11 20:43:57 +0700

    update konfigurasi homepage dan base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Tambahkan baris ini agar path file aset (.js / .css) tidak rusak saat online
+  base: '/WebsitePertama/', // Tambahkan baris ini agar path file aset (.js / .css) tidak rusak saat online
   server: {
     port: 5173,
     open: true,
